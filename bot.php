c0952c440d45b44c781392823d20100d37517f54
teesuriya <30352496+teesuriya@users.noreply.github.com>
2020-04-06 23:25:01 +0700

    ค่ะ line 27

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         $data = [
             'replyToken' => $reply_token,
             // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
+            'messages' => [['type' => 'text', 'text' => $text."ค่ะ" ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
